232c4408f374f572a6b6e5749a5f8dcd3f9e34c2
Bjarke Eriksen <bjarkede@gmail.com>
2020-05-12 16:29:56 +0200

    Move all to a single page. Add all webapi calls. Test implementation. Clean up solution.

--- I4GUI3Applikation/Scripts/data.js
+++ I4GUI3Applikation/Scripts/data.js
@@ -1,5 +1,3 @@
-JWT = ''
-
 CreateModel = Vue.component('create-model', {
     data: function () {
         return {
@@ -12,9 +10,10 @@ CreateModel = Vue.component('create-model', {
             zip: '',
             city: '',
             nationality: '',
+            birthdate: '',
             comments: '',
-            shoesize: '',
-            height: '',
+            shoesize: 0,
+            height: 0,
             eyecolor: '',
             haircolor: '',
             password: '',
@@ -45,13 +44,17 @@ CreateModel = Vue.component('create-model', {
         </div>
         <div class="form-container">
             <p>Height</p>
-            <input class="inputfield" type="text" v-model="height"/>
+            <input class="inputfield" type="number" v-model="height"/>
             <p>Shoe size</p>
-            <input class="inputfield" type="text" v-model="shoesize"/>
+            <input class="inputfield" type="number" v-model="shoesize"/>
             <p>Hair Colour</p>
             <input class="inputfield" type="text" v-model="haircolor"/>
             <p>Eye Colour</p>
             <input class="inputfield" type="text" v-model="eyecolor"/>
+            <p>Birthday</p>
+            <form>
+                <input class="inputfield" type="date" v-model="birthdate"/>
+            </form>
             <p>Comments</p>
             <form>
                 <textarea class="textfield" v-model="comments">
@@ -76,10 +79,10 @@ CreateModel = Vue.component('create-model', {
                     zip: this.zip,
                     city: this.city,
                     country: this.country,
-                    birthDate: '2020-05-11T17:52:58.314Z',
+                    birthDate: this.birthdate,
                     nationality: this.nationality,
-                    height: 0,
-                    shoeSize: 0,
+                    height: Number(this.height),
+                    shoeSize: Number(this.shoesize),
                     hairColor: this.haircolor,
                     eyeColor: this.eyecolor,
                     comments: this.comments,
@@ -93,12 +96,266 @@ CreateModel = Vue.component('create-model', {
     }
 })
 
+CreateManager = Vue.component('create-manager', {
+    data: function () {
+        return {
+            firstname: '',
+            lastname: '',
+            email: '',
+            password: '',
+        }
+    },
+    template:
+        `
+        <section class="manager-container">
+            <div class="input-container">
+                <p>Firstname</p>
+                <input class="inputfield" type="text" v-model="firstname"/>
+                <p>Lastname</p>
+                <input class="inputfield" type="text" v-model="lastname"/>
+                <p>Nationality</p>
+                <input class="inputfield" type="email" v-model="email"/>
+                <p>Email</p>
+                <input class="inputfield" type="text" v-model="password"/>
+            </div>
+            <div class="form-container">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <button class="button" v-on:click="post">Create manager</button>
+            </div>
+        </section>
+        `
+    ,
+    methods: {
+        post: function (event) {
+            axios
+                .post('https://localhost:44368/api/Managers', {
+                    firstName: this.firstname,
+                    lastName: this.lastname,
+                    email: this.email,
+                    password: this.password,
+                }).then((response) => {
+                    console.log(response);
+                }, (error) => {
+                    console.log(error);
+                });
+        }
+    }
+})
+
+CreateJob = Vue.component('create-job', {
+    data: function () {
+        return {
+            customer: '',
+            startdate: '',
+            days: 0,
+            location: '',
+            comments: '',
+        }
+    },
+    template:
+        `
+        <section class="manager-container">
+            <div class="input-container">
+                <p>Customer</p>
+                <input class="inputfield" type="text" v-model="customer"/>
+                <p>Start date</p>
+                <input class="inputfield" type="date" v-model="startdate"/>
+                <p>Days</p>
+                <input class="inputfield" type="number" v-model="days"/>
+                <p>Location</p>
+                <input class="inputfield" type="text" v-model="location"/>
+            </div>
+            <div class="form-container">
+            <p>Comments</p>
+            <form>
+                <textarea class="textfield" v-model="comments">
+                    
+                </textarea>
+            </form>
+            <br>
+            <button class="button" v-on:click="post">Create job</button>
+        </div>
+        </section>
+        `
+    ,
+    methods: {
+        post: function (event) {
+            axios
+                .post('https://localhost:44368/api/Jobs', {
+                    customer: this.customer,
+                    startDate: this.startdate,
+                    days: Number(this.days),
+                    location: this.location,
+                    comments: this.comments,
+                }).then((response) => {
+                    console.log(response);
+                }, (error) => {
+                    console.log(error);
+                });
+        }
+    }
+})
+
+AddJob = Vue.component('add-job', {
+    data: function () {
+        return {
+            jobid: 0,
+            modelid: 0,
+        }
+    },
+    template:
+        `
+        <section class="manager-container">
+            <div class="input-container">
+                <p>JobId</p>
+                <input class="inputfield" type="number" v-model="jobid"/>
+                <p>ModelId</p>
+                <input class="inputfield" type="number" v-model="modelid"/>
+            </div>
+            <div class="form-container">
+                <br>
+                <br>
+                <button class="button" v-on:click="post">Add to job</button>
+                <button class="button" v-on:click="deletejob">Delete from job</button>
+            </div>
+        </section>
+        `
+    ,
+    methods: {
+        post: function (event) {
+
+            url = 'https://localhost:44368/api/Jobs/' + this.jobid + '/model/' + this.modelid
+
+            axios
+                .post(url).then((response) => {
+                    console.log(response);
+                }, (error) => {
+                    console.log(error);
+                });
+        },
+        deletejob: function (event) {
+
+            url = 'https://localhost:44368/api/Jobs/' + this.jobid + '/model/' + this.modelid
+
+            axios
+                .delete(url).then((response) => {
+                    console.log(response);
+                }, (error) => {
+                    console.log(error);
+                });
+        }
+    }
+})
+
+GetJobs = Vue.component('get-jobs', {
+    data: function () {
+        return {
+            jobs: [],
+        }
+    },
+    template:
+        `
+        <section class="manager-container">
+        <button class="button" v-on:click="get">Update</button>
+            <ul v-for="job in jobs" style="list-style-type:none;list-style-position:inside; margin:0; padding:0;">
+                <h3>JobId: {{ job.efJobId }}</h3>
+                <li>Information: {{ job.startDate }} at: {{ job.location }}</li>
+                <li>Comments from {{ job.customer }}: {{ job.comments }}</li>
+                <li>Models: {{ job.jobModels }}</li>
+            </ul>
+        </section>
+        `
+    ,
+    methods: {
+        get: function (event) {
+            axios
+                .get('https://localhost:44368/api/Jobs').then((response) => {
+                    this.jobs = response.data;
+                    console.log(response);
+                }, (error) => {
+                    console.log(error);
+                });
+        }
+    }
+})
+
+AddExpense = Vue.component('add-expense', {
+    data: function () {
+        return {
+            jobid: 0,
+            modelid: 0,
+            date: '',
+            text: '',
+            amount: 0,
+        }
+    },
+    template:
+        `
+        <section class="manager-container">
+            <div class="input-container">
+                <p>ModelId</p>
+                <input class="inputfield" type="number" v-model="modelid"/>
+                <p>JobId</p>
+                <input class="inputfield" type="number" v-model="jobid"/>
+                <p>Date</p>
+                <input class="inputfield" type="date" v-model="date"/>
+                <p>Text</p>
+                <input class="inputfield" type="text" v-model="text"/>
+                <p>Amount</p>
+                <input class="inputfield" type="number" v-model="amount"/>
+            </div>
+            <div class="form-container">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <button class="button" v-on:click="post">Add Expense</button>
+            </div>
+        </section>
+        `
+    ,
+    methods: {
+        post: function (event) {
+            axios
+                .post('https://localhost:44368/api/Expenses', {
+                    modelId: Number(this.modelid),
+                    jobId: Number(this.jobid),
+                    date: this.date,
+                    text: this.text,
+                    amount: Number(this.amount),
+                }).then((response) => {
+                    console.log(response);
+                }, (error) => {
+                    console.log(error);
+                });
+        },
+        deletejob: function (event) {
+
+            url = 'https://localhost:44368/api/Jobs/' + this.jobid + '/model/' + this.modelid
+
+            axios
+                .delete(url).then((response) => {
+                    console.log(response);
+                }, (error) => {
+                    console.log(error);
+                });
+        }
+    }
+})
+
 LoginComponent = Vue.component('login-component', {
     data: function () {
         return {
             email: '',
             password: '',
-            oldpassword: ''
+            oldpassword: '',
         }
     },
     template: 
@@ -129,7 +386,7 @@ LoginComponent = Vue.component('login-component', {
                     password: this.password,
                     oldPassword: this.oldpassword
                 }).then((response) => {
-                    axios.defaults.headers.post['Authorization'] = 'Bearer '.concat(response.data.jwt.toString()),
+                    axios.defaults.headers.common['Authorization'] = 'Bearer '.concat(response.data.jwt.toString());
                     console.log(response);
                 }, (error) => {
                     console.log(error);
@@ -137,8 +394,4 @@ LoginComponent = Vue.component('login-component', {
         }
     }
 
-})
-
-const TestComponent = Vue.component('test-component', {
-    template: '<div><h3>Test</h3></div>'
 })
